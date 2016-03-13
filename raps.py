aecc19c100b448e2c009e5688b25d76466c153b5
Josh Jacobs <josh.jacobs19@gmail.com>
2016-03-13 20:31:48 +0000

    This is the one with the actual flag changes...

--- raps.py
+++ raps.py
@@ -114,16 +114,16 @@ def main(argv):
             print "There is nothing in the known database, please run RAPS with the install flag set."
             sys.exit()
 
-        scanint = args.interface #eventually make this a cmd flag
-        aircom = "airodump-ng --output-format csv --write %s/rapsdump %s" % (ipath, scanint + 'mon')
+        monint = args.interface + 'mon'
+        aircom = "airodump-ng --output-format csv --write %s/rapsdump %s" % (ipath, monint)
         fo = open("/proc/net/dev", 'rb')
         if fo.read().find("mon0") == -1:
-            call(['airmon-ng', 'start', scanint]) #add logic to determine which interface to put in mon
+            call(['airmon-ng', 'start', args.interface]) #add logic to determine which interface to put in mon
             time.sleep(10)
         p = Popen([aircom], stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
         time.sleep(10)
         os.kill(p.pid, SIGTERM)
-        call(['airmon-ng', 'stop', 'mon0']) #make more intelligent for different OSes?
+        call(['airmon-ng', 'stop', monint]) #make more intelligent for different OSes?
 
     if args.snmp:
         snmpAsk()
