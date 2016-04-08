61b4202ef88ef4435615d70ea1d3e5b4e2112eb0
Josh Jacobs <josh.jacobs19@gmail.com>
2016-04-08 20:23:31 +0100

    hmmm

--- raps.py
+++ raps.py
@@ -72,7 +72,7 @@ def main(argv):
         if raw_input("Do you want to organise the Unkown DB into Known and Rogue? [y/N]") == "y" or "yes" or "Y":
             for u in collu.find({}, {'SSID':1, 'BSSID':1, 'LANMAC':1, '_id':0}): #might need to delete the first bracket entirely
                 print u
-                ap = {"SSID":str(a[u'SSID']), "BSSID":str(a[u'BSSID']), "LANMAC":str(a[u'LANMAC'])}
+                ap = {"SSID":str(u[u'SSID']), "BSSID":str(u[u'BSSID']), "LANMAC":str(u[u'LANMAC'])}
                 if raw_input("Which Database Would You Like to Add This to? [k/R]") == "k":
                     collk.insert(ap)
                 else:
