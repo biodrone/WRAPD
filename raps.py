39bd460738ace159aed5a544bf8e627a2d26a085
Josh Jacobs <josh.jacobs19@gmail.com>
2016-04-08 22:06:38 +0100

    checking lanmac every time something spoopy is going on...

--- raps.py
+++ raps.py
@@ -189,6 +189,7 @@ def doTheMongo(db, collk, collu, collr, ssid, bssid):
     """
     #if lanman doesn't find anything, automatically add the AP to collu as it's not on the LAN
     lanmac = findLanMac(bssid)
+    print "Lanmac Found for SSID: %s & BSSID: %s is %s" % (ssid, bssid, lanmac)
     if lanmac == 0:
         print "AP with SSID %s, BSSID %s is not on the LAN." % (ssid, bssid)
         ap = {"SSID":ssid, "BSSID":bssid, "LANMAC":lanmac}
